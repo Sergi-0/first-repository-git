c185a5adeec18f210d1f7934a6123f36444c52ee
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2023-10-09 01:04:54 +0300

    Add files via upload

---
+++ 15(3).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int N,a;
+
+	cout << "Введите нужное условие (1 или 2): ";
+	cin >> a;
+
+	switch (a)
+	{
+
+	case 1: 
+		for (int i = 1; i < 10; ++i) 
+			cout << i << " * " << 7 << " = " << i * 7 << endl; 
+		break;
+
+	case 2:
+	{
+		cout << "Введите N: ";
+		cin >> N;
+
+		for (int i = 1; i <= 9; ++i)
+			cout << i << " x " << N << " = " << i * N << endl;
+	}
+
+	}
+}
---
+++ task14(2).cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int t;
+	using byte = int;
+	pair<byte, byte> positionA;
+	pair<byte, byte> positionB;
+	cout << "Введите координаты A(столбец,ряд) от 1 до 8: "; cin >> positionA.first >> positionA.second;
+	cout << "Введите координаты B(столбец,ряд) от 1 до 8: "; cin >> positionB.first >> positionB.second;
+	cout << "Выберите условие от 1 до 5: "; cin >> t;
+	if (t > 5) { return 0; }
+
+	switch (t)
+	{
+	case 1: if (positionA.first == positionB.first || positionA.second == positionB.second) { cout << "Ладья бьет фигуру"; break; }
+				else { cout << "Ладья не бьет по фигуре"; break; }
+
+	case 2: if (abs(positionA.first - positionB.first) == abs(positionA.second - positionB.second)) { cout << "Слон бьет фигуру"; break; }
+		  else { cout << "Слон не бьет по фигуре"; break; }
+
+	case 3: if (abs(positionA.first - positionB.first) < 2 && abs(positionA.second - positionB.second) < 2) { cout << "Король попадёт за один ход на требуемое поле"; break; }
+		  else { cout << "Чтобы попасть на требуемое поле королю нужно более одного хода"; break; }
+
+	case 4: if (abs(positionA.first - positionB.first) == abs(positionA.second - positionB.second) || positionA.first == positionB.first || positionA.second == positionB.second) { cout << "Ферзь бьет фигуру"; break; }
+		  else { cout << "Ферзь не бьет фигуру"; break; }
+
+	case 5: if ( (positionA.second == 2 && positionB.second == 4 || positionB.second - positionA.second == 1) && positionA.first == positionB.first) { cout << "Пешка может попасть на данное поле за 1 ход"; break; }
+		  else  if (positionB.second - positionA.second == 1 && abs(positionA.first - positionB.first) == 1) { cout << "Пешка может попасть на данное поле за 1 ход, при том что она съест фигуру на данном поле"; break; }
+		  else { cout << "Пешка не может попасть на данное поле"; break; }
+	}
+	
+}
