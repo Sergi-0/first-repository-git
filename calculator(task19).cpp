d41aa50fb31c61be49dd4e335d4c5172bda70b93
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2023-11-07 04:09:47 +0300

    calculator(task19).cpp

--- calculator(task19).cpp
+++ calculator(task19).cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <math.h>
 using namespace std;
@@ -6,34 +6,34 @@ using namespace std;
 string f2(string g) // ^
 {
 	int k, l;
-	
 	string numbers = "1234567890.";
 
 	while (g.find("^") != -1)
 	{
-	string fix = "";
-	string number = "";
-	string degree;
-	degree = g[g.find("^") + 1];
-	l = g.find("^") + 1;
+		string fix = "";
+		string number = "";
+		string degree;
+		degree = g[g.rfind("^") + 1];
+		l = g.rfind("^") + 1;
 
-		for (int i = g.find("^") - 1; i >= 0; --i)
+		for (int i = g.rfind("^") - 1; i >= 0; --i)
 		{
 			if (numbers.find(g[i]) == -1) break;
 			k = i;
 			number = g[i] + number;
 		}
 
-		for (int i = g.find("^") + 2; i <= g.length(); ++i)
+		for (int i = g.rfind("^") + 2; i <= g.length(); ++i)
 		{
 			if (numbers.find(g[i]) == -1) break;
 			l = i;
 			degree = degree + g[i];
 		}
 
-		fix = to_string( pow( atof( number.c_str()), atof(degree.c_str()) ) ) ;
+		fix = to_string(pow(atof(number.c_str()), atof(degree.c_str())));
 		g.replace(k, l - k + 1, fix);
-	}
+	}				
+	
 	return g;
 }
 
@@ -68,14 +68,14 @@ string f3(string g) // *
 
 		for (int i = g.find("*") + 2; i <= g.length(); ++i)
 		{
-			if (numbers.find(g[i]) == -1) break;	
+			if (numbers.find(g[i]) == -1) break;
 			l = i;
 			number2 = number2 + g[i];
 		}
 
 		fix = to_string(atof(number1.c_str()) * atof(number2.c_str()));
 		g.replace(k, l - k + 1, fix);
-	}
+	}		
 	return g;
 }
 
@@ -84,7 +84,6 @@ string f4(string g) // /
 	int k, l;
 
 	string numbers = "1234567890.";
-
 	while (g.find("/") != -1)
 	{
 		string fix = "";
@@ -92,8 +91,9 @@ string f4(string g) // /
 		string number2;
 		number2 = g[g.find("/") + 1];
 		l = g.find("/") + 1;
+		
 
-		for (int i = g.find("/") - 1; i >= 0; --i)
+		for (int i = g.find("/") - 1; i >= 0; --i) // вопрос ?????????????
 		{
 			if (numbers.find(g[i]) == -1)
 			{
@@ -107,7 +107,7 @@ string f4(string g) // /
 			k = i;
 			number1 = g[i] + number1;
 		}
-
+		
 		for (int i = g.find("/") + 2; i <= g.length(); ++i)
 		{
 			if (numbers.find(g[i]) == -1) break;
@@ -117,7 +117,7 @@ string f4(string g) // /
 
 		fix = to_string(atof(number1.c_str()) / atof(number2.c_str()));
 		g.replace(k, l - k + 1, fix);
-	}
+	}																								
 	return g;
 }
 
@@ -159,7 +159,7 @@ string f6(string g) // +
 
 		fix = to_string(atof(number1.c_str()) + atof(number2.c_str()));
 		g.replace(k, l - k + 1, fix);
-	}
+	}																													
 	return g;
 }
 
@@ -171,7 +171,7 @@ string f5(string g) // обработка строки - под f6
 
 	if (g[0] == '+') g.erase(0, 1);
 
-	for (int i = 0; i <= g.length(); ++i)
+	for (int i = 0; i < g.length(); ++i)
 	{
 		if (g[i] == '-')
 		{ 
@@ -185,6 +185,7 @@ string f5(string g) // обработка строки - под f6
 		}
 	} 
 	while (f.find("++") != -1) f.replace(f.find("++"), 2, "+");
+
 	if (f[0] == '+') f.erase(0, 1);
 
 	return f;
@@ -193,6 +194,7 @@ string f5(string g) // обработка строки - под f6
 string f1(string s) // ()
 {
 	int k;
+	
 	while (s.find(")") != -1)
 	{
 		string a = "";
@@ -205,7 +207,8 @@ string f1(string s) // ()
 		}
 		s.replace(k - 1, s.find(")") - k + 2, f6(f5(f4(f3(f2(a))))));
 	}
-	s = f6(f5(f4(f3(f2(s)))));
+	
+	s = f6(f5(f4(f3(f2(s)))));																										
 	return s;
 }
 
