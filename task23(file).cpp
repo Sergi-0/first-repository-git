307f8307f9814c946efc5d86a97171d6a450cef2
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2023-12-04 02:37:35 +0300

    task23(file).cpp

--- task23(file).cpp
+++ task23(file).cpp
@@ -1,58 +1,30 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <vector>
-#include <string>
-#include <sstream>
 
 using namespace std;
 
-struct Book
-{
-	string Author;
-	string Title;
-	int Year;
-};
-
-void saveToFile(const string& filename, const vector<Book>& data)
+void saveToFile(const string& filename, const vector<string>& data)
 {
 	ofstream file(filename);
-	if (file.is_open()) for (const auto& i : data) file << i.Author << "/" << i.Title << "/" << i.Year << " " << endl;
+	if (file.is_open()) for (const string& i : data) file << i << endl;
 	file.close();
 }
 
-void loadFromFile(const string& filename, vector<Book>& outData)
+void loadFromFile(const string& filename, vector<string>& outData)
 {
-	string k,a;
-	Book b;
+	string a;
 	ifstream file(filename);
-	if (file.is_open()) {
-		while (getline(file, a)) {
-			stringstream t(a);
-			getline(t, b.Author, '/');
-			getline(t, b.Title, '/');
-			getline(t, k, ' ');
-			b.Year = atoi(k.c_str());
-			outData.push_back(b);
-		}
-	}
+	if (file.is_open()) while (file >> a) outData.push_back(a);
 	file.close();
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	Book a, b, c;
-	a.Author = "Пушкин"; a.Title = "Сказка о рыбаке и рыбке"; a.Year = 1800;
-	b.Author = "Экзюпери"; b.Title = "Маленький принц"; b.Year = 1750;
-	c.Author = "Хемингуэй"; c.Title = "Старик и море"; c.Year = 1700;
-
-	vector<Book> data = {a,b,c};
-	vector<Book> outData;
-
+	vector<string> data = { "черепаха", "заяц", "утка" };
+	vector<string> outData;
 	saveToFile("kukuhapoehala.txt", data);
 	loadFromFile("kukuhapoehala.txt", outData);
-
-	for (int i = 0; i < 3; ++i) {
-		cout << outData[i].Author << " / " << outData[i].Title << " / " << outData[i].Year << endl;
-	}
+	cout << outData[0] << outData[1] << outData[2];
 }
